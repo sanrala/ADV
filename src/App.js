2183cbf5b9895bf797b100f8b668a26ee2b2c0f9
sanrala <sebastien.mauro@gmail.com>
2024-03-14 21:32:44 +0100

    cookies banner consentement

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import Avantage from "./WebContainer/Avantage/Avantage";
 import Presta from "./WebContainer/Presta/Presta";
 import Footer from "./WebContainer/Footer/Footer";
 import Contact from "./WebContainer/Contact/Contact";
-
+import Cookies from "./WebContainer/Cookies/Cookies";
 function App() {
   return (
     <div className="App">
       <Nav />
- 
+ <Cookies/>
       <main id="main">
         <About />
         <Avantage />
---
+++ src/WebContainer/Cookies/Cookies.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import './CookieBar.css';
+
+const CookieBar = () => {
+  const [accepted, setAccepted] = useState(false);
+
+  const acceptCookies = () => {
+    localStorage.setItem('cookiesAccepted', 'true');
+    setAccepted(true);
+  };
+
+  const declineCookies = () => {
+    localStorage.setItem('cookiesDecline', 'false');
+    setAccepted(true);
+  };
+
+  const cookiesAccepted = localStorage.getItem('cookiesAccepted') === 'true';
+  const cookiesDecline = localStorage.getItem('cookiesDecline') === 'false';
+  if (accepted || cookiesAccepted) {
+    return null; // Hide the bar if cookies are accepted
+  }
+
+  return (
+    <div class="cookie-consent-banner">
+    <div class="cookie-consent-banner__inner">
+      <div class="cookie-consent-banner__copy">
+        <div class="cookie-consent-banner__header">CE SITE UTILISE DES COOKIES</div>
+        <div class="cookie-consent-banner__description">Nous utilisons des cookies pour personnaliser le contenu et les publicités, pour fournir des fonctionnalités de médias sociaux et pour analyser notre trafic. Nous partageons également des informations sur votre utilisation de notre site avec nos partenaires de médias sociaux, de publicité et d’analyse qui peuvent les combiner avec d’autres informations que vous leur avez fournies ou qu’ils ont collectées lors de votre utilisation de leurs services. Vous consentez à nos cookies si vous continuez à utiliser notre site Web.</div>
+      </div>
+  
+      <div class="d-flex cookie-consent-banner__actions">
+        <a href="#" class="cookie-consent-banner__cta" onClick={acceptCookies}>
+          Accepter
+        </a>
+        
+        <a href="#" class="cookie-consent-banner__cta cookie-consent-banner__cta--secondary" onClick={declineCookies}>
+          Refuser
+        </a>
+      </div>
+    </div>
+  </div>
+  );
+};
+
+export default CookieBar;
